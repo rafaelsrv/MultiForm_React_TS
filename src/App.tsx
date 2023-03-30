87154fb0e83a6817250e790ed916a08f88b8893e
Rafael <rafael-srv@hotmail.com>
2023-03-30 13:26:55 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { FormStep1 } from './pages/FormStep1/';
+import { FormStep2 } from './pages/FormStep2';
+import { FormStep3 } from './pages/FormStep3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App (){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<FormStep1/>} />
+                <Route path="/step2" element={<FormStep2/>} />
+                <Route path="/step3" element={<FormStep3/>} />
+            </Routes>    
+        </BrowserRouter>
+
+    );
 }
-
-export default App;
+export default App
