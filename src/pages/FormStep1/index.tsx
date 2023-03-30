36b48819c840d030afaab4d813d481c79e77aa72
Rafael <rafael-srv@hotmail.com>
2023-03-30 19:38:14 -0300

    Usando Context

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,9 +1,44 @@
+import { useNavigate } from 'react-router-dom'
 import * as C from './styles'
+import { useForm, FormActions } from '../../contexts/FormContext'
+import { Theme } from '../../components/Theme'
+import { ChangeEvent } from 'react'
+
 
 export const FormStep1 = ()=>{
+    const navigate = useNavigate();
+    const { state, dispatch} = useForm();
+
+    const handleNextStep = () =>{
+        navigate('/step2');
+    }
+    
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        })
+    }
+
     return(
-        <C.Container>
-           Passo 1
-        </C.Container>
+        <Theme>
+            <C.Container>
+                <p>Passo 1/3 - {state.name}</p>
+                <h1>Vamos começar com seu nome</h1>
+                <p>Preencha o campo abaixo com seu nome completo</p>
+                <hr/>
+
+                <label>
+                    Seu nome completo
+                    <input
+                        type='text'
+                        autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}/>
+                </label>
+
+                <button onClick={handleNextStep}> Próximo </button>
+            </C.Container>
+        </Theme>       
     )
 }
--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,9 +1,11 @@
 import * as C from './styles'
+import { Theme } from '../../components/Theme'
 
 export const FormStep2 = ()=>{
     return(
-        <C.Container>
-            Passo 2
-        </C.Container>
+        <Theme>
+            
+        </Theme>
+        
     )
 }
