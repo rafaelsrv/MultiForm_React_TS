e41abb3e75f2c103c5c7ac673612f2645e81c501
Rafael <rafael-srv@hotmail.com>
2023-03-30 20:30:41 -0300

    Passo 2 concluído

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -2,15 +2,28 @@ import { useNavigate } from 'react-router-dom'
 import * as C from './styles'
 import { useForm, FormActions } from '../../contexts/FormContext'
 import { Theme } from '../../components/Theme'
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useEffect } from 'react'
 
 
 export const FormStep1 = ()=>{
     const navigate = useNavigate();
     const { state, dispatch} = useForm();
 
+    useEffect(()=>{
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+
+        })
+    },[])
+
     const handleNextStep = () =>{
-        navigate('/step2');
+        if(state.name !== ''){
+            navigate('/step2');
+        } else{
+            alert('Preencha os dados')
+        }
+        
     }
     
     const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -23,7 +36,7 @@ export const FormStep1 = ()=>{
     return(
         <Theme>
             <C.Container>
-                <p>Passo 1/3 - {state.name}</p>
+                <p>Passo 1/3</p>
                 <h1>Vamos começar com seu nome</h1>
                 <p>Preencha o campo abaixo com seu nome completo</p>
                 <hr/>
--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,11 +1,71 @@
+import { useNavigate } from 'react-router-dom'
 import * as C from './styles'
+import { useForm, FormActions } from '../../contexts/FormContext'
 import { Theme } from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react'
+import { SelectOption } from '../../components/SelectOption'
+
 
 export const FormStep2 = ()=>{
+    const navigate = useNavigate();
+    const { state, dispatch} = useForm();
+
+    useEffect(()=>{
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+
+        })
+    },[])
+
+    const handleNextStep = () =>{
+        if(state.name !== ''){
+            navigate('/step2');
+        } else{
+            alert('Preencha os dados')
+        }
+        
+    }
+    
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        })
+    }
+    
+    const setLevel = (level:number) =>{
+        dispatch({
+            type: FormActions.setLevel,
+            payload: level
+        })
+    }
+
     return(
         <Theme>
-            
-        </Theme>
-        
+            <C.Container>
+                <p>Passo 2/3</p>
+                <h1>{state.name}, o que melhor descreve você?</h1>
+                <p>O que melhor descreve sua experiência com programação?</p>
+                <hr/>
+
+               <SelectOption
+                    title="Sou iniciante"
+                    description="Comecei a programar há menos de 2 anos"
+                    icon="🥳"
+                    selected={state.level === 0}
+                    onClick={()=>setLevel(0)}
+               />
+               <SelectOption
+                    title="Sou programador"
+                    description="Já programo há 2 anos ou mais"
+                    icon="😎"
+                    selected={state.level === 1}
+                    onClick={()=>setLevel(1)}
+               />
+
+                <button onClick={handleNextStep}> Próximo </button>
+            </C.Container>
+        </Theme>       
     )
 }
